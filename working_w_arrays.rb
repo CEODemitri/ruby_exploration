58abfe73e008b11746e7f6561256eec837b241a1
Color Codex <colorcodex8@Colors-iMac.local>
2023-09-17 10:09:03 -0500

    working with arrays

---
+++ working_w_arrays.rb
@@ -0,0 +1,43 @@
+# An Array is an Object that holds other Objects
+# defined order and can store all kinds of objects
+
+# this array has five elements: a string, a number, true, a symbol, and another number
+array = ["A String", 1, true, :symbol, 2]
+
+
+# SIDE NOTE < SYMBOLS! :: THEY ARE LIKE STRINGS BUT I BELIEVE SAVE MEMORY:: YES< SYMBOLS ARE FASTER THEN STRINGS BECAUSE THY TAKE UP LESS MEMORY SPACE AND PROVIDE FASTER EXECUTION
+# EX. each will out put a different Object id, thus consuming additional space
+    puts "hello".object_id
+    puts "hello".object_id
+    puts "hello".object_id
+        # symbols use one instance and makes copies
+        puts :hello.object_id
+        puts :hello.object_id
+        puts :hello.object_id
+
+
+# Array
+# two dimensional or nested array that represents the structure of number keys on a phone
+[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+# use index to access objects and also liek below
+puts [1, 2, 3].first
+puts [1, 2, 3].last
+
+# accessing object that does not exist results in
+numbers = [1, 2, 3]
+puts numbers[3]
+# OUTPUT: nil
+
+# but we can set the new object into a new position 
+numbers[3] = 4
+puts numbers
+
+# overwrite existing object
+numbers[0] = "uno"
+puts numbers
