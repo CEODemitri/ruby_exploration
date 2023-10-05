3a33bb0dbf6f46d47feaae6a147f742ea757654b
CEODemitri <colorcodex8@Colors-iMac.local>
2023-10-05 08:05:58 -0500

    operators

---
+++ objects/Methods/Operators/operators.rb
@@ -0,0 +1,25 @@
+# arithmetic operators: addition, subtraction, multiplication, division, exponentiation, modulo
+# particulary for numbers but some may be fitted on strings and other objects
+
+
+# logical operators: focus on main three (and, or, not)
+# and 
+    # always appears between two values and returns true if BOTH values are true
+
+
+# or
+    # returns true if at least one value is true
+
+
+# not
+    # operates on only one operand, returns the neagted opposite value
+
+
+
+# Operator Precedence
+# each three comes in two versions
+    # and , &&
+    # or , ||
+    # not, !
+
+    # the symbols are prioritized over the word operators
