df7c219bc7314b4ea37490bde5d25929913567ba
ceoDemitri <demichole@gmail.com>
2023-12-13 23:06:12 -0600

    attribute writers: learning how to implement attribute writers with classes and the purpose of doing so.

--- Classes/atttribute.rb
+++ Classes/atttribute.rb
@@ -17,3 +17,27 @@ puts person.name
 # attrribute readers are methods that do nothing but return a value assigned to an instance variable with the same name
 # another way to look at it: attribute readers are methods that expose anin stance variable
 # the name attribute reader is weird, just know thats the name
+
+# attribute writers
+class Blogger
+    def initialize(name)
+        @name = name 
+    end
+
+    def name
+        @name
+    end
+
+    def password=(password)
+        @password = password
+    end
+end
+# password= method does nothing else but take a single arguement and assign the value of this local variable to the instance @password variable
+# now use the attribute writer
+blogger = Blogger.new("Bebo")
+blogger.password=("pass word")
+p blogger
+
+# or 
+puts blogger2 = Blogger.new("Myspace")
+puts blogger2.password = "another"
