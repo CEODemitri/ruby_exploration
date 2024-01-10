a41816765b113f1da173837df9a22ab3428c6b9a
ceoDemitri <demichole@gmail.com>
2024-01-10 06:23:30 -0600

    Methods: practice with strings and arrays. Also to use method.sort to find a list of possible methods for the object

---
+++ xtras/other_methods.rb
@@ -0,0 +1,18 @@
+# many names of methods defined on strings and arrays will describe exactly what they are doing
+
+p 'butter and eggs'.capitalize
+# returns strin with first letter capital
+
+p 'butter and eggs'.length
+
+p [52, 1, 7].sort
+# sort the given array and return with elements in order
+
+p nums = [1, 5, 5, 3, 2].uniq
+p nums.include?(2)
+
+
+# how to find methods to choose from or possiblities?
+string = "okay"
+p string.methods.sort
+p nums.methods.sort
