88ff90cb57e26546f3e7ffd8f4c1c08f3fef449d
ceoDemitri <demichole@gmail.com>
2024-04-02 07:58:06 -0500

    little more logic to the character builder cli

--- SCRIPTS/character_builder.rb
+++ SCRIPTS/character_builder.rb
@@ -10,8 +10,16 @@ puts "What is your name: "
 name = gets.chomp
 
 greetings = ["Hallo ", "Que Pasa ", "My Round ", "Wifey ", "Bonjour ", "Salve ", "Konnichiwa "]
-puts greetings.sample + name
-
-puts "What type of Character Would You Like to Build"
-character = gets.chomp
+puts greetings.sample + name + "! Welcome to Character Builder. We have a suit of four characters to begin."
 puts " "
+puts "Ninja, Tech, Fantasy, Elemental"
+puts " "
+puts " "
+
+puts "What type of Character Would You Like to Build?"
+character = gets.chomp
+character.titleize!
+puts " "
+
+confirm = ["Cool", "Perfect", "Interesting Choice", "Powerful"]
+puts confirm.sample + "! You selected " + character + ". Let's Begin."
