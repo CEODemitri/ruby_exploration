168fc81b76fa3628e48c21b6fa7dc25f6947a4a9
ceoDemitri <demichole@gmail.com>
2024-08-31 09:54:26 -0500

    completes first chapter Using Libraries

--- webapps_4_beginners/README.md
+++ webapps_4_beginners/README.md
@@ -1,10 +1,17 @@
 # Webapps For Beginners
-- [] Using Libraries
+- [ x ] Using Libraries
 1. Ruby Standard Library
     a. working with a few packages: Date
 2. RubyGems
 3. Bundler
 4. Ruby Load Path
+Completed
+
+## TODO
+- [ ] Exercises: Using Rubygems
+- [ ] Using Bundler
+
+
 - [] HTML
 - [] Embedded Ruby
 - [] HTTP
--- webapps_4_beginners/Using_Libraries/ruby_gems.rb
+++ webapps_4_beginners/Using_Libraries/ruby_gems.rb
@@ -1,6 +1,11 @@
 # gem is a library prepared to be distributed via the internet. 
-# provide certain tools for solvinf certain problems
+# provide certain tools for solving certain problems
 require "middlemen"
 
 
 # two sites, 10min reads
+
+# ---
+# Some gems are not built in and require using gem cli tool
+# install the gem and then require it for use.
+
