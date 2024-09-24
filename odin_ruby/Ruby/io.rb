b15e207154839fb0def3d7d37798a2862443242b
ceoDemitri <demichole@gmail.com>
2024-09-23 22:02:40 -0500

    ruby io with odin project

---
+++ odin_ruby/Ruby/io.rb
@@ -0,0 +1,23 @@
+# notes
+# print vs puts
+# print keeps things on one line
+# puts appends a new line to the arguement passed in
+
+# small examples
+print "Learning is FUN!"
+puts "Learning is cool."
+
+# gets
+# user input
+gets
+# play input
+# => "play input\n" <-- return statement
+# gets always return new line at end of input, often makes use of a “separator” to read streams and multi-line files
+# use chomp
+new_string = gets.chomp
+# play input
+# => "play input"
+
+
+# ---
+# assignment
