eee2e068bf19b8ed207f154ca60646d2f09f75ad
ceoDemitri <demichole@gmail.com>
2024-10-21 06:53:57 -0500

    conditional logic start

---
+++ odin_ruby/Ruby/conditional.rb
@@ -0,0 +1,20 @@
+# Introduction
+# example
+if room_tidy == true
+  "I can play games"
+end
+
+# 0. will always have an expression that evaluates to true or false
+
+# ---
+# Lesson Overview
+# 0. describe and list falsy values
+# 1. how to use if, elsif, and else
+# 2. explain difference between if and unless
+# 3. ||, &&, !
+# 4. short circuit evaluation
+# 5. ternary operator
+# 6. case statement
+
+# ---
+# Truthy and falsy in Ruby
