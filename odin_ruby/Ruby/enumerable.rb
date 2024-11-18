44b6163952b5df72d466e9cf971f95ffad2286a6
ceoDemitri <demichole@gmail.com>
2024-11-18 06:43:38 -0600

    start of enumerables

---
+++ odin_ruby/Ruby/enumerable.rb
@@ -0,0 +1,2 @@
+# **enumerables** set of convenient built in methods in ruby that are included as part of both arrays and hashes
+# enumberable docs https://docs.ruby-lang.org/en/3.3/Enumerable.html
