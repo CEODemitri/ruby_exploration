b24bace2c31188294624287b6e87d44c50cd4c3d
ceoDemitri <demichole@gmail.com>
2025-01-06 06:30:50 -0600

    any? predicate enumerable lesson

--- odin_ruby/Ruby/predicate_enumerable.rb
+++ odin_ruby/Ruby/predicate_enumerable.rb
@@ -16,3 +16,7 @@ p select_even.include?(numbers[5]) # => false
 # returns true if any element in array or hash match the condition within the block
 p numbers.any? { |number| number > 500 }
 p numbers.any? { |number| number < 20 }
+
+#
+# all?
+# only returns true if all elements in array or hash match condition within the block
