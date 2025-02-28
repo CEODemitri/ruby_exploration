2e978bd441f50328d3201c365e39121c6c99d534
ceoDemitri <demichole@gmail.com>
2025-02-27 23:39:39 -0600

    finishes predicate enumerables

--- odin_ruby/Ruby/predicate_enumerable.rb
+++ odin_ruby/Ruby/predicate_enumerable.rb
@@ -20,3 +20,38 @@ p numbers.any? { |number| number < 20 }
 #
 # all?
 # only returns true if all elements in array or hash match condition within the block
+# long ex using eeach
+# check whether 3 char or 6 char
+fruits = ["apple", "grape", "lemon", "strawberry", 'kiwi']
+matches = []
+fruits.each do |fruit|
+  if fruit.length > 3
+    matches.push(fruit)
+  end
+end
+
+results = fruits.length == matches.length
+puts results #=> true
+
+# reset matches and results to check if more than six
+matches = []
+results = false
+fruits.each do |fruit|
+  if fruit.length > 6
+    matches.push(fruit)
+  end
+end
+
+results = fruits.length == matches.length
+puts results #=> false
+
+# using all 
+fruits.all? { |fruit| fruit.length > 3 } #=> true
+fruits.all? { |fruit| fruit.length > 6 } #=> false
+
+# all? returns true on default unless block returns false or nil
+
+# none
+# returns true if condition  in block matches none of the elements in array or hash
+fruits.none? { |fruit| fruit.length > 10 } #=> true
+fruits.none? { |fruit| fruit.length > 6 } #=> false
